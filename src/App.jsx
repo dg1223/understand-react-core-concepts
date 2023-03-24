2687133e2118da45382a1e08a6aed828f926cb1d
dg1223 <alavi1223@hotmail.com>
2023-03-24 00:52:48 -0400

    state vs props, stateful vs stateless

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Watch from "./components/Watch/Watch";
+import Knob from "./components/Knob/Knob";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Knob steps="456"></Knob>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
